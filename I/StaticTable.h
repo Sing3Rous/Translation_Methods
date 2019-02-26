5b5fa7b239597a09d1b067594aec502253bdf2e4
David <singerous@gmail.com>
2019-02-26 15:16:26 +0700

    created StaticTable.h
    Static table class' header file

---
+++ I/StaticTable.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class StaticTable {
+public:
+	StaticTable() = default;
+	StaticTable(const string &fileName);
+	~StaticTable() = default;
+
+	void init(const vector <string> &_data);
+	void upload(const string &fileName);
+	int find(const string &unit) const;
+	
+
+private:
+	vector <string> data;
+};
