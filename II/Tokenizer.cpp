c975c992adff3071058b40132a831554347b9534
David <singerous@gmail.com>
2019-03-26 20:10:17 +0700

    updated tokenizer.cpp
    added some spaces and changed identificators -> identifiers
    updated parse function: deleted useless crunch part
    added tokenizer error log and added writing in this log

--- II/Tokenizer.cpp
+++ II/Tokenizer.cpp
@@ -228,7 +228,7 @@ WhichTableToAdd DFA::getTableType(void) {
 
 	if (state == ADD || state == PUSH || state == LESS || state == MORE || state == NAME || state == NUMBER) {
 
-		if (symbol == LETTER) return KEYWORDS_OR_IDENTIFICATORS;
+		if (symbol == LETTER) return KEYWORDS_OR_IDENTIFIERS;
 		if (symbol == DELIMETER || symbol == END_OF_LINE) return DELIMETERS;
 		if (symbol == NUMERAL) return CONSTANTS;
 		if (symbol == ARITHMETIC_OPERATION || symbol == LESS_SYMBOL || symbol == MORE_SYMBOL ||
@@ -250,10 +250,12 @@ int DFA::getPos(void) const {
 }
 
 vector <Token> DFA::parse(StaticTable &keyWords, StaticTable &delimiters,
-	StaticTable &operations, DynamicTable &constants, DynamicTable &identificators) {
+	StaticTable &operations, DynamicTable &constants, DynamicTable &identifiers) {
 
 	vector <Token> tokens;
 	Token token;
+
+	ofstream out("tokenizer_error_log.txt");
 
 	while (this->automating()) {
 
@@ -263,14 +265,14 @@ vector <Token> DFA::parse(StaticTable &keyWords, StaticTable &delimiters,
 
 			switch (this->getTableType()) {
 
-			case KEYWORDS_OR_IDENTIFICATORS:
+			case KEYWORDS_OR_IDENTIFIERS:
 
 				if (keyWords.find(lex) == -1) {
 
-					identificators.add(lex);
-					token.tableName = "Identificators";
-					token.firstPos = identificators.find(lex).first;
-					token.secondPos = identificators.find(lex).second;
+					identifiers.add(lex);
+					token.tableName = "Identifiers";
+					token.firstPos = identifiers.find(lex).first;
+					token.secondPos = identifiers.find(lex).second;
 					token.lexem = lex;
 					tokens.push_back(token);
 				} else {
@@ -310,6 +312,7 @@ vector <Token> DFA::parse(StaticTable &keyWords, StaticTable &delimiters,
 				break;
 			case ERR:
 
+				out << "Error state on lexem: " << lexem << endl;
 				break;
 			case IGNORE:
 
@@ -318,63 +321,6 @@ vector <Token> DFA::parse(StaticTable &keyWords, StaticTable &delimiters,
 		}
 	}
 
-	auto lex = this->getLexem();
-
-	switch (this->getTableType()) {
-
-	case KEYWORDS_OR_IDENTIFICATORS:
-
-		if (keyWords.find(lex) == -1) {
-
-			identificators.add(lex);
-			token.tableName = "Identificators";
-			token.firstPos = identificators.find(lex).first;
-			token.secondPos = identificators.find(lex).second;
-			token.lexem = lex;
-			tokens.push_back(token);
-		}
-		else {
-
-			token.tableName = "Key Words";
-			token.firstPos = keyWords.find(lex);
-			token.secondPos = -1;
-			token.lexem = lex;
-			tokens.push_back(token);
-		}
-		break;
-	case DELIMETERS:
-
-		token.tableName = "Delimeters";
-		token.firstPos = delimiters.find(lex);
-		token.secondPos = -1;
-		token.lexem = lex;
-		tokens.push_back(token);
-		break;
-	case CONSTANTS:
-
-		constants.add(lex);
-		token.tableName = "Constants";
-		token.firstPos = constants.find(lex).first;
-		token.secondPos = constants.find(lex).second;
-		token.lexem = lex;
-		tokens.push_back(token);
-
-		break;
-	case OPERATIONS:
-
-		token.tableName = "Operations";
-		token.firstPos = operations.find(lex);
-		token.secondPos = -1;
-		token.lexem = lex;
-		tokens.push_back(token);
-		break;
-	case ERR:
-
-		break;
-	case IGNORE:
-
-		break;
-	}
-
+	out.close();
 	return tokens;
 }
